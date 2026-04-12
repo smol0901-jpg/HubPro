f1865296f86528d19f593f5e435bb3b87c65a71d
smol0901-jpg <smol0901@gmail.com>
2026-04-12 17:10:32 +0300

    Add preload.js with database API

--- preload.js
+++ preload.js
@@ -1,8 +1,27 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
+  // База данных
+  getBots: () => ipcRenderer.invoke('db:getBots'),
+  addBot: (data) => ipcRenderer.invoke('db:addBot', data),
+  updateBot: (data) => ipcRenderer.invoke('db:updateBot', data),
+  deleteBot: (id) => ipcRenderer.invoke('db:deleteBot', id),
+  
+  getGroups: () => ipcRenderer.invoke('db:getGroups'),
+  addGroup: (data) => ipcRenderer.invoke('db:addGroup', data),
+  deleteGroup: (id) => ipcRenderer.invoke('db:deleteGroup', id),
+  
+  getMessages: (groupId) => ipcRenderer.invoke('db:getMessages', groupId),
+  addMessage: (data) => ipcRenderer.invoke('db:addMessage', data),
+  
+  getStats: () => ipcRenderer.invoke('db:getStats'),
+  
+  // Telegram
   sendTelegramMessage: (data) => ipcRenderer.invoke('tg:send', data),
-  syncBotConfig: (config) => ipcRenderer.send('tg:sync-config', config),
+  checkBot: (token) => ipcRenderer.invoke('tg:checkBot', token),
+  syncBotConfig: () => ipcRenderer.send('tg:sync-config'),
+  
+  // События
   onTelegramUpdate: (cb) => ipcRenderer.on('tg:incoming', (e, d) => cb(d)),
   onUpdateAvailable: (cb) => ipcRenderer.on('update-available', (e, i) => cb(i)),
   onUpdateDownloaded: (cb) => ipcRenderer.on('update-downloaded', (e, i) => cb(i)),
