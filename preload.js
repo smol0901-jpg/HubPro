2f3b3d5872449e3ee2aa33b02f0255124ddccdd1
smol0901-jpg <smol0901@gmail.com>
2026-04-15 17:16:28 +0300

    Update preload.js with new API endpoints

--- preload.js
+++ preload.js
@@ -1,32 +1,40 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
+  // ВЕРСИЯ
+  getVersion: () => ipcRenderer.invoke('app:getVersion'),
+  
   // АВТОРИЗАЦИЯ
   login: (data) => ipcRenderer.invoke('auth:login', data),
   getUsers: () => ipcRenderer.invoke('auth:getUsers'),
   addUser: (data) => ipcRenderer.invoke('auth:addUser', data),
+  updateUser: (data) => ipcRenderer.invoke('auth:updateUser', data),
   requestDeleteUser: (id) => ipcRenderer.invoke('auth:requestDelete', id),
   approveDeleteUser: (id) => ipcRenderer.invoke('auth:approveDelete', id),
   cancelDeleteUser: (id) => ipcRenderer.invoke('auth:cancelDelete', id),
   
-  // База данных - БОТЫ
+  // БОТЫ
   getBots: () => ipcRenderer.invoke('db:getBots'),
   addBot: (data) => ipcRenderer.invoke('db:addBot', data),
   updateBot: (data) => ipcRenderer.invoke('db:updateBot', data),
   deleteBot: (id) => ipcRenderer.invoke('db:deleteBot', id),
   
-  // База данных - ГРУППЫ
+  // ГРУППЫ
   getGroups: () => ipcRenderer.invoke('db:getGroups'),
   addGroup: (data) => ipcRenderer.invoke('db:addGroup', data),
+  updateGroup: (data) => ipcRenderer.invoke('db:updateGroup', data),
   deleteGroup: (id) => ipcRenderer.invoke('db:deleteGroup', id),
   
-  // База данных - СООБЩЕНИЯ
+  // СООБЩЕНИЯ
   getMessages: (groupId) => ipcRenderer.invoke('db:getMessages', groupId),
   addMessage: (data) => ipcRenderer.invoke('db:addMessage', data),
+  clearMessages: (groupId) => ipcRenderer.invoke('db:clearMessages', groupId),
   
-  // База данных - РАСПИСАНИЕ
+  // РАСПИСАНИЕ
   getSchedules: () => ipcRenderer.invoke('db:getSchedules'),
+  getScheduleLogs: (scheduleId) => ipcRenderer.invoke('db:getScheduleLogs', scheduleId),
   addSchedule: (data) => ipcRenderer.invoke('db:addSchedule', data),
+  updateSchedule: (data) => ipcRenderer.invoke('db:updateSchedule', data),
   deleteSchedule: (id) => ipcRenderer.invoke('db:deleteSchedule', id),
   toggleSchedule: (data) => ipcRenderer.invoke('db:toggleSchedule', data),
   
