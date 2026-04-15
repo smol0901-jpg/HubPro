a8afb1e3b291a1f9578aa362a857ecaf0082d6f0
smol0901-jpg <smol0901@gmail.com>
2026-04-15 20:12:18 +0300

    Update preload.js with export/import and topics API

--- preload.js
+++ preload.js
@@ -1,7 +1,8 @@
 const { contextBridge, ipcRenderer } = require('electron');
-
 contextBridge.exposeInMainWorld('electronAPI', {
-  // АВТОРИЗАЦИЯ
+  getVersion: () => ipcRenderer.invoke('app:getVersion'),
+  exportData: () => ipcRenderer.invoke('data:export'),
+  importData: (data) => ipcRenderer.invoke('data:import', data),
   login: (data) => ipcRenderer.invoke('auth:login', data),
   getUsers: () => ipcRenderer.invoke('auth:getUsers'),
   addUser: (data) => ipcRenderer.invoke('auth:addUser', data),
@@ -9,43 +10,28 @@ contextBridge.exposeInMainWorld('electronAPI', {
   requestDeleteUser: (id) => ipcRenderer.invoke('auth:requestDelete', id),
   approveDeleteUser: (id) => ipcRenderer.invoke('auth:approveDelete', id),
   cancelDeleteUser: (id) => ipcRenderer.invoke('auth:cancelDelete', id),
-  
-  // База данных - БОТЫ
   getBots: () => ipcRenderer.invoke('db:getBots'),
   addBot: (data) => ipcRenderer.invoke('db:addBot', data),
   updateBot: (data) => ipcRenderer.invoke('db:updateBot', data),
   deleteBot: (id) => ipcRenderer.invoke('db:deleteBot', id),
-  
-  // База данных - ГРУППЫ
   getGroups: () => ipcRenderer.invoke('db:getGroups'),
   addGroup: (data) => ipcRenderer.invoke('db:addGroup', data),
   updateGroup: (data) => ipcRenderer.invoke('db:updateGroup', data),
   deleteGroup: (id) => ipcRenderer.invoke('db:deleteGroup', id),
-  
-  // База данных - СООБЩЕНИЯ
   getMessages: (groupId) => ipcRenderer.invoke('db:getMessages', groupId),
   addMessage: (data) => ipcRenderer.invoke('db:addMessage', data),
   clearMessages: (groupId) => ipcRenderer.invoke('db:clearMessages', groupId),
-  
-  // База данных - РАСПИСАНИЕ
   getSchedules: () => ipcRenderer.invoke('db:getSchedules'),
+  getScheduleLogs: (scheduleId) => ipcRenderer.invoke('db:getScheduleLogs', scheduleId),
   addSchedule: (data) => ipcRenderer.invoke('db:addSchedule', data),
   updateSchedule: (data) => ipcRenderer.invoke('db:updateSchedule', data),
   deleteSchedule: (id) => ipcRenderer.invoke('db:deleteSchedule', id),
   toggleSchedule: (data) => ipcRenderer.invoke('db:toggleSchedule', data),
-  
-  // Статистика
   getStats: () => ipcRenderer.invoke('db:getStats'),
-  
-  // Версия
-  getVersion: () => ipcRenderer.invoke('app:getVersion'),
-  
-  // Telegram API
   sendTelegramMessage: (data) => ipcRenderer.invoke('tg:send', data),
   checkBot: (token) => ipcRenderer.invoke('tg:checkBot', token),
+  getTopics: (data) => ipcRenderer.invoke('tg:getTopics', data),
   syncBotConfig: () => ipcRenderer.send('tg:sync-config'),
-  
-  // События
   onTelegramUpdate: (cb) => ipcRenderer.on('tg:incoming', (e, d) => cb(d)),
   onScheduleExecuted: (cb) => ipcRenderer.on('tg:scheduleExecuted', (e, d) => cb(d)),
   onUpdateAvailable: (cb) => ipcRenderer.on('update-available', (e, i) => cb(i)),
