dbdde0888d246344a29af25097c5ade538564734
smol0901-jpg <smol0901@gmail.com>
2026-05-10 19:52:32 +0300

    Добавление главного процесса Electron с поддержкой dev и production режимов

---
+++ electron/main.ts
@@ -0,0 +1,29 @@
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+
+const isDev = !app.isPackaged;
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+
+  if (isDev) {
+    win.loadURL('http://localhost:3000');
+  } else {
+    win.loadFile(path.join(__dirname, '../out/index.html'));
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
